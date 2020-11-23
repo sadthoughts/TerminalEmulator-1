87916ae8327f9c675422df034405a9aed9f29e64
sadthoughts <74781317+sadthoughts@users.noreply.github.com>
2020-11-23 10:32:27 +0300

    Add files via upload

---
+++ TerminalEmulator/TerminalEmulator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace TerminalEmulator
+{
+    public class TerminalEmulator
+    {
+        private Catalog users;
+        private Catalog enable;
+        private Catalog config;
+
+        public TerminalEmulator()
+        {
+            DirectoryInitializer();
+        }
+
+
+        private void DirectoryInitializer()
+        {
+            UserListCommand();
+            EnableListCommand();
+            ConfigListCommand();
+        }
+        private void UserListCommand()
+        {
+            users = new Catalog("User");
+            users.AddCommand("time", () => Console.WriteLine(DateTime.Now));
+            users.AddCommand("enable", () => enable.Operation());
+        }
+        private void EnableListCommand()
+        {
+            enable = new Catalog("Enable", users.path);
+            enable.AddCommand("config", () => config.Operation());
+
+        }
+        private void ConfigListCommand()
+        {
+            config = new Catalog("Config", enable.path);
+            config.AddCommand("colorgreen", () => Console.ForegroundColor = ConsoleColor.Green);
+        }
+        public void RunningTerminal() { users.Operation(); }
+
+
+    }
+
+}
